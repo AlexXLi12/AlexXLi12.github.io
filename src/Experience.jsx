99f97d8e517bb0417f0bd957b5f7edc72d87def4
Alex Li <alexxuanqili@gmail.com>
2024-12-24 20:19:14 -0500

    responsive styling

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -2,28 +2,44 @@ import "./Experience.css";
 import Position from "./Position";
 
 const Experience = () => {
-    const positions = [
-        {
-            title: "Undergraduate Researcher",
-            subtitle: "UW Sensor Systems Lab",
-            description: "Currently working on research in the UW Sensor Systems Lab under Jared Nakahara on acoustic levitation.",
-            technologies: ["SystemVerilog", "FPGAs"]
-        },
-        {
-            title: "Software Engineering Intern",
-            subtitle: "WEX, Inc.",
-            description: "Worked on an internal chatbot tool to streamline the meeting scheduling process.",
-            technologies: ["Google Cloud Platform", "Google Chat API", "Python", "Flask"]
-        }
-    ];
+	const positions = [
+		{
+			title: "Undergraduate Researcher",
+			subtitle: "UW Sensor Systems Lab",
+			description:
+				"Working on research in the UW Sensor Systems Lab under Jared Nakahara on acoustic levitation. Specifically, I am developing Verilog code to control signalling between hardware components.",
+			technologies: ["SystemVerilog", "FPGAs"],
+		},
+		{
+			title: "Software Engineering Intern",
+			subtitle: "WEX, Inc.",
+			description:
+				"Worked on an internal chatbot tool to streamline the meeting scheduling process using Calendar API, Chat API, and Google Cloud Platform.",
+			technologies: [
+				"Google Cloud Platform",
+				"Google Chat API",
+				"Python",
+				"Flask",
+			],
+		},
+		{
+			title: "Research Intern",
+			subtitle:
+				"George Mason University - College of Engineering and Computing",
+			description:
+				"Implemented and conducted experiments on the novel Sharpness-Aware Minimization Optimizer to measure improvements over Stochastic Gradient Descent on the CIFAR-10 image classification dataset.",
+			technologies: ["Python", "PyTorch"],
+		},
+	];
 
-    return (
-        <div className="Experience">
-            {positions.map((position, index) => (
-                <Position key={index} {...position} />
-            ))}
-        </div>
-    );
+	return (
+		<div className="experience" id="experience">
+			<h1>Experience</h1>
+			{positions.map((position, index) => (
+				<Position key={index} {...position} />
+			))}
+		</div>
+	);
 };
 
 export default Experience;
--- src/Jumbotron.jsx
+++ src/Jumbotron.jsx
@@ -2,11 +2,13 @@ import "./Jumbotron.css";
 import About from "./About";
 const Jumbotron = () => {
 	return (
-		<div className="jumbotron" id='about'>
-			<div className="element" style={{ flexDirection: "row" }}>
-				<img className="portrait shadow-effect" src="./images/portrait.jpg" alt="Portrait" />
-				<About />
-			</div>
+		<div className="jumbotron" id="about">
+			<img
+				className="portrait shadow-effect"
+				src="./images/portrait.jpg"
+				alt="Portrait"
+			/>
+			<About />
 		</div>
 	);
 };
